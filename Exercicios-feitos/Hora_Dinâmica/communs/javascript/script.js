a3a20e234e29b017462da6a944f32a13488f2c76
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2024-10-11 03:45:13 -0300

    manutenção no caminho de alguns elementos

--- Exercicios-feitos/Hora_Dinâmica/communs/javascript/script.js
+++ Exercicios-feitos/Hora_Dinâmica/communs/javascript/script.js
@@ -10,26 +10,25 @@ var corpo = window.document.getElementsByTagName('body')
 function carregar() {
     var agr = new Date()
     var hora = agr.getHours()
-    
 
     if (hora > 0 && hora < 12) {
         
         msg.innerHTML = `Bom dia agora são <strong>${hora}H am<strong>`
-        img.src = '/exercicos/Hora/midias/imagens/manha.png'
+        img.src = '/midias/imagens/manha.png'
         document.body.style.background = '#ddca9f' // para colorir o fundo do a tag body
 
     }
     else if (hora < 18) {
         
         msg.innerHTML = `Boa Tarde agora são <strong>${hora}H pm<strong>`
-        img.src = '/exercicos/Hora/midias/imagens/tarde.png'
+        img.src = '/midias/imagens/tarde.png'
         document.body.style.background = '#9b5e3f'
 
     }
     else if (hora >= 18 && hora < 24) {
 
         msg.innerHTML = `Boa Noite agora são <strong>${hora}H pm<strong> `
-        img.src = '/exercicos/Hora/midias/imagens/noite.png'
+        img.src = '/midias/imagens/noite.png'
         document.body.style.background = '#0b1a1f'
 
     }
@@ -38,7 +37,7 @@ function carregar() {
         console.log('[ERROR] horario invalido')
 
     }
-   
+
 }
 
 
